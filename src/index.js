c1e3b35af916784794f5526569155a2bd9e6e78f
kshiitz1111 <kshitizghimire8@gmail.com>
2025-01-29 12:42:44 +0545

    scattered iris flower plot

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { getData } from "./iris_data.js";
+import { select } from "d3";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = select('body').append('svg').attr('width', width).attr('height', height);
+
+let data;
+try {
+  data = await getData(width, height);
+} catch (error) {
+  console.error(error)
+}
+console.log(data)
+svg.selectAll('circle')
+  .data(data)
+  .join('circle')
+  .attr('r', 5)
+  .attr('cx', (d) => d.x)
+  .attr('cy', (d) => d.y)
---
+++ src/iris_data.js
@@ -0,0 +1,51 @@
+import { csv, extent, scaleLinear } from "d3"
+
+
+const csvUrl = [
+  'https://gist.githubusercontent.com/',
+  'curran/',
+  'a08a1080b88344b0c8a7/',
+  'raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/',
+  'iris.csv'
+].join('')
+
+const parseRow = (d) => {
+  d.sepal_length = +d.sepal_length;
+  d.sepal_width = +d.sepal_width;
+  d.petal_length = +d.petal_length;
+  d.petal_width = +d.petal_width;
+  return d;
+}
+
+const xValue = (d) => d.petal_length;
+const yValue = (d) => d.sepal_length;
+
+async function getDataFromCsvUrl() {
+  try {
+    const data = await csv(csvUrl, parseRow);
+    // console.log("CSV Data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching CSV:", error);
+    return undefined;
+  }
+}
+
+export const getData = async (weight, height) => {
+  if (getDataFromCsvUrl()) {
+    const data = await getDataFromCsvUrl();
+
+    const x = scaleLinear().domain(extent(data, xValue)).range([0, weight]);
+    const y = scaleLinear().domain(extent(data, yValue)).range([height, 0]);
+
+    const marks = data.map(d => ({
+      x: x(d.petal_length),
+      y: y(d.sepal_length)
+    }))
+    return marks;
+  }
+
+  if (!getDataFromCsvUrl()) {
+    return null;
+  }
+}
